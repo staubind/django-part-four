563e6a2dc2053924c7c6d645510f1d8cad385a41
Dan St. Aubin <staubindj@gmail.com>
2021-09-04 22:04:28 -0500

    finished about half of the content for part four

--- learning_templates/basic_app/urls.py
+++ learning_templates/basic_app/urls.py
@@ -1,1 +1,11 @@
 from django.urls import path
+from basic_app import views
+
+# so that we can point to our paths here using relative paths
+app_name = 'basic_app' # always set this to whatever the application's name is
+# allows us to use template tagging.
+
+urlpatterns = [
+    path('relative/', views.relative, name='relative'),
+    path('other/', views.other, name='other'),
+]
